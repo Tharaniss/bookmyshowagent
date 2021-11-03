ec7593cc228cb3a61d89110ea2bf14063d6e98c0
Tharani Sankaran <t.sankaran@lakeba.com>
2021-11-03 16:00:39 +0530

    Test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ site += '20211104'
 venue = 'INPT' # Inorbit Mall
 showtime = ''
 showList = ''
-delay = 100 # Delays the script execution
+delay = 20 # Delays the script execution
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.77 Chrome/70.0.3538.77 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
 
 # Configure the email account (if you need an email notification)
